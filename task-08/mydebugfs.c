ad6357f585a4ce3272a8b1ee458e9ac8b9b5ce39
Clement T <me@trosa.io>
2020-03-19 22:58:32 +0100

    task8

--- task-08/mydebugfs.c
+++ task-08/mydebugfs.c
@@ -1,10 +1,12 @@
 /* TASK_08 */
 
 #include <linux/fs.h>
+#include <linux/timer.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/uaccess.h>
 #include <linux/debugfs.h>
+#include <linux/jiffies.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("My eudyptula debugfs implementation");
@@ -15,11 +17,6 @@ MODULE_AUTHOR("IOMONAD");
 static struct dentry *mentry = NULL;
 
 /* `id` file implementation */
-
-static int id_open(struct inode *ip, struct file *fp)
-{
-	return 0;
-}
 
 static ssize_t id_read(struct file *fp, char __user *user, size_t size,
 		       loff_t *loff)
@@ -49,39 +46,19 @@ static ssize_t id_write(struct file *fp, const char __user *buffer, size_t size,
 	}
 }
 
-static struct file_operations id_fops = { .open = id_open,
-					  .read = id_read,
-					  .write = id_write };
+static struct file_operations id_fops = { .read = id_read, .write = id_write };
 
-/* `jiffries` file implementation */
+/* `jiffies` file implementation */
 
-static int jiffries_open(struct inode *ip, struct file *fp)
+static ssize_t jiffies_read(struct file *fp, char __user *user, size_t size,
+			    loff_t *loff)
 {
 	return 0;
 }
 
-static ssize_t jiffries_read(struct file *fp, char __user *user, size_t size,
-			     loff_t *loff)
-{
-	return 0;
-}
-
-static ssize_t jiffries_write(struct file *fp, const char __user *buffer,
-			      size_t size, loff_t *loff)
-{
-	return 0;
-}
-
-static struct file_operations jiffries_fops = { .open = jiffries_open,
-						.read = jiffries_read,
-						.write = jiffries_write };
+static struct file_operations jiffies_fops = { .read = jiffies_read };
 
 /* `foo` file implementation */
-
-static int foo_open(struct inode *ip, struct file *fp)
-{
-	return 0;
-}
 
 static ssize_t foo_read(struct file *fp, char __user *user, size_t size,
 			loff_t *loff)
@@ -95,26 +72,25 @@ static ssize_t foo_write(struct file *fp, const char __user *buffer,
 	return 0;
 }
 
-static struct file_operations foo_fops = { .open = foo_open,
-					   .read = foo_read,
+static struct file_operations foo_fops = { .read = foo_read,
 					   .write = foo_write };
 
 /** MODULE CORE */
 
 static __init int initialize(void)
 {
-	struct dentry *id, *jiffries, *foo;
+	struct dentry *id, *jiffies, *foo;
 
 	if ((mentry = debugfs_create_dir(DEBUGFS_FOLDER_NAME, NULL)) == NULL) {
 		printk(KERN_ALERT "Error while creating debugfs root folder");
 		return 1;
 	}
-	if ((id = debugfs_create_file("id", 777, mentry, NULL, &id_fops)) ==
+	if ((id = debugfs_create_file("id", 666, mentry, NULL, &id_fops)) ==
 	    NULL) {
 		return 1;
 	}
-	if ((jiffries = debugfs_create_file("jiffries", 777, mentry, NULL,
-					    &jiffries_fops)) == NULL) {
+	if ((jiffies = debugfs_create_file("jiffies", 444, mentry, NULL,
+					   &jiffies_fops)) == NULL) {
 		return 1;
 	}
 	if ((foo = debugfs_create_file("foo", 777, mentry, NULL, &foo_fops)) ==
