249bb2694705eb0411161037e914ff0cfe8cb7c6
Clement T <me@trosa.io>
2020-03-26 11:53:52 +0100

    Linked List impl are OK

--- task-12/identity.c
+++ task-12/identity.c
@@ -1,4 +1,4 @@
-/* TASK_12 */
+/* LEVEL12 */
 
 #include <linux/init.h>
 #include <linux/module.h>
@@ -9,28 +9,28 @@
 #include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("TASK12");
+MODULE_DESCRIPTION("Linked List Kernel Impl");
 MODULE_AUTHOR("IOMONAD");
 
-#define NAME_SIZE 20
+#define NAME_LEN 20
 
-/* struct indentity { */
-/* 	char name[NAME_SIZE]; */
-/* 	int id; */
-/* 	bool busy; */
-/* 	struct list_head curr; */
-/* }; */
+struct identity {
+	char name[NAME_LEN];
+	int id;
+	bool busy;
+	struct list_head lst;
+};
 
 static LIST_HEAD(identity_list);
 
-/* FIND */
+/* SEARCH */
 struct identity *identity_find(int id)
 {
-	struct identity *cursor = NULL;
+	struct identity *it = NULL;
 
-	list_for_each_entry (cursor, &identity_list, curr) {
-		if (cursor->id == id) {
-			return cursor;
+	list_for_each_entry (it, &identity_list, lst) {
+		if (it->id == id) {
+			return it;
 		}
 	}
 	return NULL;
@@ -41,73 +41,67 @@ int identity_create(char *name, int id)
 {
 	struct identity *new = NULL;
 
-	if (identity_find(id)) {
-		pr_info("identity: id '%d' already exists.", id);
+	if (identity_find(id) != NULL) {
+		/* ID EXISTS */
 		return 1;
 	}
-	if (name == NULL)
-		return 1;
-	if (!(new = (struct identity *)kmalloc(sizeof(struct identity),
-					       GFP_KERNEL))) {
-		return -ENOMEM;
+	if (!(new = kmalloc(sizeof(*new), GFP_KERNEL))) {
+		/* ALLOC FAILED */
+		return 2;
 	}
-	strncpy(new->name, name, NAME_SIZE);
+	memset(new->name, 0x00, NAME_LEN);
+	strncpy(new->name, name, NAME_LEN - 1);
 	new->id = id;
 	new->busy = false;
-	list_add(&(new->list), &identity_list);
+	list_add(&(new->lst), &identity_list);
+	pr_info("identity %d: with '%s' name was created\n", id, name);
 	return 0;
 }
 
+/* DELETE */
 void identity_destroy(int id)
 {
-	struct indentity *destroy = NULL;
+	struct identity *temp = NULL;
 
-	if (!(destroy = identity_find(id))) {
-		pr_debug("identity: id '%d' not found while destroy", id);
+	if (!(temp = identity_find(id))) {
 		return;
 	}
-	list_del(&(destroy->curr));
-	if (destroy) {
-		kfree(destroy);
-	}
+	list_del(&(temp->lst));
+	kfree(temp);
 	return;
 }
 
 static __init int initialize(void)
 {
-	struct identity *temp;
+	struct identity *temp = NULL;
 
-	if (identity_create("Alice", 1)) {
+	if (identity_create("Alice", 1))
 		pr_debug("error creating Alice\n");
-	}
-	if (identity_create("Bob", 2)) {
+	if (identity_create("Bob", 2))
 		pr_debug("error creating Bob\n");
-	}
-	if (identity_create("Dave", 3)) {
+	if (identity_create("Dave", 3))
 		pr_debug("error creating Dave\n");
-	}
-	if (identity_create("Gena", 10)) {
+	if (identity_create("Gena", 10))
 		pr_debug("error creating Gena\n");
-	}
 
 	temp = identity_find(3);
 	pr_debug("id 3 = %s\n", temp->name);
 
-	if (!(temp = identity_find(42))) {
+	temp = identity_find(42);
+	if (temp == NULL)
 		pr_debug("id 42 not found\n");
-	}
 
 	identity_destroy(2);
 	identity_destroy(1);
 	identity_destroy(10);
 	identity_destroy(42);
 	identity_destroy(3);
+
 	return 0;
 }
 
 static __exit void destroy(void)
 {
-	return;
 }
 
 module_init(initialize);
