7150ce1b0d464d71b19499a55b4b3fb5ddc635f7
Clement T <me@trosa.io>
2020-03-28 17:27:24 +0100

    x64 syscall implementation

---
+++ task-15/sys_eudyptula_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+
+int
+main(int argc, char *argv) {
+	printf("OK");
+	printf("%d\n", syscall(439,1,1)); /* WRONG */
+	printf("%d\n", syscall(439,0xDEACE042,0xDEAD)); /* GOOD */
+	return 0;
+}
