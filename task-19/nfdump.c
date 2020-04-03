e08b46f64c6a1c59741a0def801e32d8642ab69d
Clement T <me@trosa.io>
2020-04-03 13:35:36 +0200

    updated dumper

--- task-19/nfdump.c
+++ task-19/nfdump.c
@@ -3,15 +3,52 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/version.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+static struct nf_hook_ops nho;
+
+static unsigned int custom_nf_dump_hk(
+	void *priv, struct sk_buff *skb,
+	const struct nf_hook_state *state)
+{
+
+	pr_info("packet size: %d", 3); /* FIXME */
+	return NF_ACCEPT;
+}
+
+static int nf_dump_init(void)
+{
+	nho.hook = custom_nf_dump_hk;
+	nho.hooknum = 0;	/* ALL INCOMMING */
+	nho.pf = PF_INET;
+	nho.priority = NF_IP_PRI_FIRST;
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+	return nf_register_net_hook(&init_net, &nho);
+#else
+	return nf_register_hook(&nho);
+#endif
+}
 
 static __init int initialize(void)
 {
+	if (nf_dump_init()) {
+		pr_info("nfdump: Error while registering nf hook.");
+		return -EINVAL;
+	}
 	return 0;
 }
 
 static __exit void destroy(void)
 {
-	return ;
+	return
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+		nf_unregister_net_hook(&init_net, &nho);
+#else
+	        nf_unregister_hook(&nho);
+#endif
 }
 
 module_init(initialize);
