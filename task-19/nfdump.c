55e0dbda642413b98cc0b2cd0722330cba828bc1
Clement T <me@trosa.io>
2020-04-03 19:11:19 +0200

    Dumper done with kernel textsearch toolkit

--- task-19/nfdump.c
+++ task-19/nfdump.c
@@ -5,27 +5,32 @@
 #include <linux/kernel.h>
 #include <linux/version.h>
 #include <linux/netfilter.h>
+#include <linux/textsearch.h>
 #include <linux/netfilter_ipv4.h>
 
+#define PATTERN "iomonad_computorz_lord"
+
 static struct nf_hook_ops nho;
+static struct ts_config *tsc = NULL;
 
-static unsigned int custom_nf_dump_hk(
-	void *priv, struct sk_buff *skb,
-	const struct nf_hook_state *state)
+static unsigned int custom_nf_dump_hk(void *priv, struct sk_buff *skb,
+				      const struct nf_hook_state *state)
 {
-
-	pr_info("packet size: %d", 3); /* FIXME */
+	if (skb_find_text(skb, 0, skb->len, tsc) != UINT_MAX) {
+		pr_info("Found matching pattern in packet (size=%d): %s !!",
+			skb->len, PATTERN);
+	}
 	return NF_ACCEPT;
 }
 
 static int nf_dump_init(void)
 {
 	nho.hook = custom_nf_dump_hk;
-	nho.hooknum = 0;	/* ALL INCOMMING */
+	nho.hooknum = NF_INET_PRE_ROUTING;
 	nho.pf = PF_INET;
 	nho.priority = NF_IP_PRI_FIRST;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)
 	return nf_register_net_hook(&init_net, &nho);
 #else
 	return nf_register_hook(&nho);
@@ -34,6 +39,10 @@ static int nf_dump_init(void)
 
 static __init int initialize(void)
 {
+	tsc = textsearch_prepare("kmp", PATTERN, strlen(PATTERN), GFP_KERNEL,
+				 TS_AUTOLOAD);
+	if (IS_ERR(tsc))
+		return PTR_ERR(tsc);
 	if (nf_dump_init()) {
 		pr_info("nfdump: Error while registering nf hook.");
 		return -EINVAL;
@@ -43,11 +52,12 @@ static __init int initialize(void)
 
 static __exit void destroy(void)
 {
+	textsearch_destroy(tsc);
 	return
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)
 		nf_unregister_net_hook(&init_net, &nho);
 #else
-	        nf_unregister_hook(&nho);
+		nf_unregister_hook(&nho);
 #endif
 }
 
--- task-19/readme.md
+++ task-19/readme.md
@@ -1,1 +1,6 @@
 # Kernel Netfilter Module
+
+
+```
+[ 4281.741738] Found matching pattern in packet (size=143): iomonad_computorz_lord !!
+```
